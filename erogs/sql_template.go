a8a4cc5505ba101eae378f02e7858477e431bc39
tommy125 <hhhvftghf@gmail.com>
2025-09-25 11:14:53 +0800

    chore: add a basic sql build function of erogs-FuzzySearchMusic

--- erogs/sql_template.go
+++ erogs/sql_template.go
@@ -67,3 +67,121 @@ FROM (
     LIMIT 1
 ) AS c;`, result), nil
 }
+
+func buildFuzzySearchMusicSQL(search string) (string, error) {
+	search = strings.ReplaceAll(search, "'", "''")
+	result := "%"
+	if utils.IsAllEnglish(search) {
+		result += search + "%"
+	} else {
+		for _, r := range search {
+			result += string(r) + "%"
+		}
+	}
+
+	if strings.TrimSpace(search) == "" {
+		return "", internalerrors.ErrSearchNoContent
+	}
+
+	return fmt.Sprintf(`
+WITH singer_agg AS (
+    SELECT music AS music_id,
+           json_agg(
+               json_build_object(
+                   'singer_name', c.name
+               )
+           ) AS singers
+    FROM singer
+    JOIN createrlist AS c ON c.id = singer.creater
+    GROUP BY music
+),
+lyrics_agg AS (
+    SELECT music AS music_id,
+           json_agg(
+               json_build_object(
+                   'lyric_name', c.name
+               )
+           ) AS lyrics
+    FROM lyrics
+    JOIN createrlist AS c ON c.id = lyrics.creater
+    GROUP BY music
+),
+arrangement_agg AS (
+    SELECT music AS music_id,
+           json_agg(
+               json_build_object(
+                   'arrangement_name', c.name
+               )
+           ) AS arrangements
+    FROM arrangement
+    JOIN createrlist AS c ON c.id = arrangement.creater
+    GROUP BY music
+),
+composition_agg AS (
+    SELECT music AS music_id,
+           json_agg(
+               json_build_object(
+                   'composition_name', c.name
+               )
+           ) AS compositions
+    FROM composition
+    JOIN createrlist AS c ON c.id = composition.creater
+    GROUP BY music
+),
+gamelist_agg AS (
+    SELECT gm.music AS music_id,
+           json_agg(
+               json_build_object(
+                   'game_name', g.gamename,
+                   'category', gm.category
+               )
+           ) AS games
+    FROM game_music gm
+    JOIN gamelist g ON g.id = gm.game
+    GROUP BY gm.music
+),
+musicitemlist_agg AS (
+    SELECT music AS music_id,
+           json_agg(
+               json_build_object(
+                   'album_name', mi.name
+               )
+           ) AS albums
+    FROM musicitem_music mim
+    JOIN musicitemlist mi ON mi.id = mim.musicitem
+    GROUP BY music
+),
+usermusic_tokuten_agg AS (
+    SELECT music AS music_id,
+           ROUND(AVG(LEAST(tokuten, 100)),2) AS avg_tokuten,
+           COUNT(uid) AS tokuten_count
+    FROM usermusic_tokuten
+    GROUP BY music
+)
+SELECT json_agg(row_to_json(t))
+FROM (
+    SELECT m.id AS music_id,
+           m.name AS songname,
+           m.playtime,
+           m.releasedate,
+           ut.avg_tokuten,
+           ut.tokuten_count,
+           s.singers,
+           l.lyrics,
+           a.arrangements,
+           comp.compositions,
+           g.games,
+           mi.albums
+    FROM musiclist m
+    LEFT JOIN singer_agg s ON s.music_id = m.id
+    LEFT JOIN lyrics_agg l ON l.music_id = m.id
+    LEFT JOIN arrangement_agg a ON a.music_id = m.id
+    LEFT JOIN composition_agg comp ON comp.music_id = m.id
+    LEFT JOIN gamelist_agg g ON g.music_id = m.id
+    LEFT JOIN musicitemlist_agg mi ON mi.music_id = m.id
+    LEFT JOIN usermusic_tokuten_agg ut ON ut.music_id = m.id 
+    WHERE m.name ILIKE '%s'
+    LIMIT 2000
+) t;
+`, result), nil
+}
