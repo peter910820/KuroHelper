32fefaa670a90fed1da74be87010d1006c77dafb
tommy125 <hhhvftghf@gmail.com>
2025-09-27 21:21:14 +0800

    fix(style): fix typo of erogs模糊查詢遊戲

--- bot/interaction.go
+++ bot/interaction.go
@@ -35,7 +35,7 @@ func onInteractionApplicationCommand(s *discordgo.Session, i *discordgo.Interact
 		go handlers.ErogsFuzzySearchCreator(s, i, nil)
 	case "erogs模糊查詢音樂":
 		go handlers.ErogsFuzzySearchMusic(s, i, nil)
-	case "erogs模糊查詢遊戲ˇ":
+	case "erogs模糊查詢遊戲":
 		go handlers.ErogsFuzzySearchGame(s, i, nil)
 	}
 }
