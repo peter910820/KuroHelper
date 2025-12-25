106cebb4a6b6e611e5072bc35cfb75d1a2de9a9f
SeaotterMS <peter910820@gmail.com>
2025-12-25 19:07:19 +0800

    feat: update userdata related feature

--- handlers/add_has_played.go
+++ handlers/add_has_played.go
@@ -52,146 +52,117 @@ func AddHasPlayed(s *discordgo.Session, i *discordgo.InteractionCreate, cid *uti
 		userID := utils.GetUserID(i)
 		userName := utils.GetUsername(i)
 		if strings.TrimSpace(userID) != "" && strings.TrimSpace(userName) != "" {
-			var msg string
-			// 先檢查該筆以及操作是不是已經執行過
-			gameRecord, err := kurohelperdb.GetUserGameErogs(userID, res.ID)
+			err := kurohelperdb.Dbs.Transaction(func(tx *gorm.DB) error {
+				// 1. 確保 User 存在
+				if _, err := kurohelperdb.EnsureUserTx(tx, userID, userName); err != nil {
+					return err
+				}
+
+				// 2. 確保 Brand 存在
+				if _, err := kurohelperdb.EnsureBrandErogsTx(tx, res.BrandID, res.BrandName); err != nil {
+					return err
+				}
+
+				// 3. 確保 Game 存在
+				if _, err := kurohelperdb.EnsureGameErogsTx(tx, res.ID, res.Gamename, res.BrandID); err != nil {
+					return err
+				}
+
+				// 4. 建立資料
+				if err := kurohelperdb.CreateUserHasPlayedTx(tx, userID, res.ID, completeDate); err != nil {
+					return err
+				}
+
+				return nil // commit
+			})
 			if err != nil {
-				// 沒有資料 開始新建
-				if errors.Is(err, gorm.ErrRecordNotFound) {
-					apiModel := kurohelperdb.UpsertUserGameErogsTXInput{
-						UserID:         userID,
-						UserName:       userName,
-						ErogsBrandID:   res.BrandID,
-						ErogsBrandName: res.BrandName,
-						ErogsGameID:    res.ID,
-						ErogsGamename:  res.Gamename,
-						HasPlayed:      true,
-						InWish:         false,
-						CompleteDate:   completeDate,
-					}
-					err = kurohelperdb.UpsertUserGameErogsTransaction(apiModel)
-					if err != nil {
-						utils.HandleError(err, s, i)
-						return
-					}
-				} else {
-					utils.HandleError(err, s, i)
-					return
-				}
-			} else if gameRecord.HasPlayed { // 有資料的狀況下，hasPlayed已被設定過
-				embed := &discordgo.MessageEmbed{
-					Title: "資料已建立，本次動作無效",
-					Color: 0x7BA23F,
-				}
-				utils.InteractionEmbedRespondForSelf(s, i, embed, nil, true)
+				utils.HandleError(err, s, i)
 				return
-			} else { // 有資料，hasPlayed還沒被設定過
-				apiModel := kurohelperdb.UpsertUserGameErogsTXInput{
-					UserID:         userID,
-					UserName:       userName,
-					ErogsBrandID:   res.BrandID,
-					ErogsBrandName: res.BrandName,
-					ErogsGameID:    res.ID,
-					ErogsGamename:  res.Gamename,
-					HasPlayed:      true,
-					InWish:         gameRecord.InWish,
-					CompleteDate:   completeDate,
-				}
-				err = kurohelperdb.UpsertUserGameErogsTransaction(apiModel)
-				if err != nil {
-					utils.HandleError(err, s, i)
-					return
-				}
 			}
 
+			// 確保新建立的使用者有加入快取
 			if _, ok := cache.UserCache[userID]; !ok {
 				cache.UserCache[userID] = struct{}{}
 			}
 
-			if msg == "" {
-				msg = "加入成功！"
-			}
-
 			embed := &discordgo.MessageEmbed{
-				Title: msg,
+				Title: "加入成功！",
 				Color: 0x7BA23F,
 			}
 			utils.InteractionEmbedRespondForSelf(s, i, embed, nil, true)
-			return
-		} else {
+		} else { // 找不到使用者，此狀況應該會是Discord官方問題或是程式碼邏輯問題
 			embed := &discordgo.MessageEmbed{
 				Title: "找不到使用者！",
 				Color: 0x7BA23F,
 			}
 			utils.InteractionEmbedRespondForSelf(s, i, embed, nil, true)
-			return
 		}
-	}
+	} else {
+		var res *erogs.FuzzySearchGameResponse
 
-	var res *erogs.FuzzySearchGameResponse
-
-	keyword, err := utils.GetOptions(i, "keyword")
-	if err != nil {
-		utils.HandleError(err, s, i)
-		return
-	}
-
-	completeDate, err := utils.GetOptions(i, "complete_date")
-	if err != nil && !errors.Is(err, kurohelpererrors.ErrOptionNotFound) {
-		utils.HandleError(err, s, i)
-		return
-	}
-
-	var t time.Time
-	if completeDate != "" {
-		t, err = utils.ParseYYYYMMDD(completeDate)
+		keyword, err := utils.GetOptions(i, "keyword")
 		if err != nil {
 			utils.HandleError(err, s, i)
 			return
 		}
 
-		if t.After(time.Now().AddDate(0, 0, 1)) {
+		completeDate, err := utils.GetOptions(i, "complete_date")
+		if err != nil && !errors.Is(err, kurohelpererrors.ErrOptionNotFound) {
 			utils.HandleError(err, s, i)
 			return
 		}
-	}
 
-	idSearch, _ := regexp.MatchString(`^e\d+$`, keyword)
-	res, err = erogs.GetGameByFuzzy(keyword, idSearch)
-	if err != nil {
-		utils.HandleError(err, s, i)
-		return
-	}
+		var t time.Time
+		if completeDate != "" {
+			t, err = utils.ParseYYYYMMDD(completeDate)
+			if err != nil {
+				utils.HandleError(err, s, i)
+				return
+			}
 
-	idStr := uuid.New().String()
-	cache.Set(idStr, *res)
+			if t.After(time.Now().AddDate(0, 0, 1)) {
+				utils.HandleError(err, s, i)
+				return
+			}
+		}
 
-	cidCommandName := utils.MakeCIDCommandName(i.ApplicationCommandData().Name, false, "")
-	messageComponent := []discordgo.MessageComponent{utils.MakeCIDAddHasPlayedComponent("✅", idStr, t, cidCommandName)}
-	actionsRow := utils.MakeActionsRow(messageComponent)
+		idSearch, _ := regexp.MatchString(`^e\d+$`, keyword)
+		res, err = erogs.GetGameByFuzzy(keyword, idSearch)
+		if err != nil {
+			utils.HandleError(err, s, i)
+			return
+		}
 
-	image := generateImage(i, res.BannerUrl)
+		idStr := uuid.New().String()
+		cache.Set(idStr, *res)
 
-	embed := &discordgo.MessageEmbed{
-		Author: &discordgo.MessageEmbedAuthor{
-			Name: res.BrandName,
-		},
-		Title: fmt.Sprintf("**%s(%s)**", res.Gamename, res.SellDay),
-		URL:   res.Shoukai,
-		Color: 0x7BA23F,
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:   "發行機種",
-				Value:  res.Model,
-				Inline: false,
+		cidCommandName := utils.MakeCIDCommandName(i.ApplicationCommandData().Name, false, "")
+		messageComponent := []discordgo.MessageComponent{utils.MakeCIDAddHasPlayedComponent("✅", idStr, t, cidCommandName)}
+		actionsRow := utils.MakeActionsRow(messageComponent)
+
+		image := generateImage(i, res.BannerUrl)
+
+		embed := &discordgo.MessageEmbed{
+			Author: &discordgo.MessageEmbedAuthor{
+				Name: res.BrandName,
 			},
-			{
-				Name:   "確認",
-				Value:  "你確定要加入已玩嗎?",
-				Inline: false,
+			Title: fmt.Sprintf("**%s(%s)**", res.Gamename, res.SellDay),
+			URL:   res.Shoukai,
+			Color: 0x7BA23F,
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "發行機種",
+					Value:  res.Model,
+					Inline: false,
+				},
+				{
+					Name:   "確認",
+					Value:  "你確定要加入已玩嗎?",
+					Inline: false,
+				},
 			},
-		},
-		Image: image,
+			Image: image,
+		}
+		utils.InteractionEmbedRespondForSelf(s, i, embed, actionsRow, true)
 	}
-	utils.InteractionEmbedRespondForSelf(s, i, embed, actionsRow, true)
 }
